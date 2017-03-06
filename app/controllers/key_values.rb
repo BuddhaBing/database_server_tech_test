a5d2804804878cec19fdae1d3f3ae4eb5c155154
Rob Brentnall <rob.brentnall@gmail.com>
2017-03-06 12:52:07 +0000

    added a success message for pair storage

--- app/controllers/key_values.rb
+++ app/controllers/key_values.rb
@@ -1,11 +1,17 @@
 class DatabaseServer < Sinatra::Base
 
   get '/set' do
-
+    if !params.empty?
+      @key_val = KeyValue.instance || KeyValue.create
+      @key_val.pairs[params.keys.first] = params.values.first
+      "Key value pair stored"
+    end
   end
 
   get '/get' do
-
+    if !params.empty? && params.keys.first == "key"
+      @key_val = KeyValue.instance || KeyValue.create
+      @key_val.pairs[params.values.first]
+    end
   end
-
 end
--- spec/features/storing_keys_spec.rb
+++ spec/features/storing_keys_spec.rb
@@ -1,10 +1,20 @@
-feature 'storing a key/value pair' do
-  it 'I can visit the "set" URL' do
-    visit '/set'
-    expect(page.status_code).to be(200)
+feature 'Storing and retrieving key/value pairs' do
+  context 'storing a key/value pair' do
+    it 'I can visit the "set" URL' do
+      visit '/set'
+      expect(page.status_code).to be(200)
+      expect(page).not_to have_content("Key value pair stored")
+    end
+    it 'I can store a key value pair by passing them as URL params' do
+      visit '/set?somekey=someval'
+      expect(page.status_code).to be(200)
+      expect(page).to have_content("Key value pair stored")
+    end
   end
-  it 'I can visit the "get" URL' do
-    visit '/get'
-    expect(page.status_code).to be(200)
+  context 'retrieving a value from a key' do
+    it 'I can visit the "get" URL' do
+      visit '/get'
+      expect(page.status_code).to be(200)
+    end
   end
 end
